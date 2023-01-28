1430c0e8cc71569fc27c489cf91c62920bf1302e
Donald Fletcher <donaldfletcher11@outlook.com>
2023-01-28 14:09:26 -0800

    js into webpage

---
+++ webpage.js
@@ -0,0 +1,10 @@
+let usersName = prompt ("What is your name")
+//console.log("Hi " +usersName + " welcome to my page!");
+document.write ("Hi " + usersName + " welcome to my page!");
+
+if (usersName == "kassie", "Kassie"){
+    alert("We love code fellows!");
+} else {
+    alert("We love JavaScript");
+}
+
