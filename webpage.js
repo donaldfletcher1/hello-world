5b765cbe46dd3646c228a56639bd4b528018cd4c
Donald Fletcher <donaldfletcher11@outlook.com>
2023-01-28 16:12:04 -0800

    functions

--- webpage.js
+++ webpage.js
@@ -1,10 +1,22 @@
-let usersName = prompt ("What is your name")
+let usersName = prompt("What is your name")
 //console.log("Hi " +usersName + " welcome to my page!");
-document.write ("Hi " + usersName + " welcome to my page!");
+document.write = prompt("Hi " + usersName + " welcome to my page!");
 
-if (usersName == "kassie", "Kassie"){
+if (usersName = "kassie", "Kassie") {
     alert("We love code fellows!");
 } else {
     alert("We love JavaScript");
 }
+function userAge() {
+    prompt("How old are you?")
+}
+userAge();
 
+function Name() {
+    let name = prompt("what is your name?").trim();
+    if (name === "") {
+
+        prompt("What is really your name?")
+    }
+}
+Name();
