0c075bd6a4c60acdd3291a8bc6607a30e64d1d2c
Donald Fletcher <donaldfletcher11@outlook.com>
2023-01-30 15:51:39 -0800

    updated changes

--- webpage.js
+++ webpage.js
@@ -2,7 +2,7 @@ let usersName = prompt("What is your name")
 //console.log("Hi " +usersName + " welcome to my page!");
 //document.write = prompt("Hi " + usersName + " welcome to my page!");
 
-if (usersName = "kassie", "Kassie") {
+if (usersName == "kassie" ||usersName == "Kassie") {
     alert("We love code fellows!");
 } else {
     alert("We love JavaScript");
@@ -12,9 +12,18 @@ function userAge() {
 }
 userAge();
 
+function Name() {
+    let name = prompt("what is your name?").trim();
+    if (name === "") {
+
+        prompt("What is really your name?")
+    }
+}
+Name();
+
 let userNum = prompt("Pick a Number!) (Between 1 & 5 Please") 
 for(let i = 0; i <userNum; i++){
-    document.write ( <img src="joker.jpg" />);
+    document.write ( '<img src="joker.jpg" />');
 }
 
 
